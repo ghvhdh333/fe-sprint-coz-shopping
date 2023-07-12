c60691371fa313f534e24a72be5bb42fb7e6dec5
김우주 <https://github.com/ghvhdh333>
2023-07-12 19:36:27 +0900

    feat/불필요한코드삭제및초기구조화

--- my-app/src/components/dropdown.js
+++ my-app/src/components/dropdown.js
@@ -1,15 +1,15 @@
-// import './dropdown.css';
+import './dropdown.css';
 
-// export default function Dropdown () {
-//     return (
-//         <section>
-//             <div>
-//                 <ul>
-//                     <li>○○○님, 안녕하세요!</li>
-//                     <li><img src="dropdown_product_icon.png" alt="product_icon"></img>상품리스트 페이지</li>
-//                     <li><img src="dropdown_bookmark_icon.png" alt="bookmark_icon"></img>북마크 페이지</li>
-//                 </ul>
-//             </div>
-//         </section>
-//     )
-// }
+export default function Dropdown () {
+    return (
+        <section>
+            <div>
+                <ul>
+                    <li>김우주님, 안녕하세요!</li>
+                    <li><img src="dropdown_product_icon.png" alt="product_icon"></img>상품리스트 페이지</li>
+                    <li><img src="dropdown_bookmark_icon.png" alt="bookmark_icon"></img>북마크 페이지</li>
+                </ul>
+            </div>
+        </section>
+    )
+}
